5a9b0002e5393b8e037d48106295ceecc4b129d1
kane <kanedev@live.fr>
2015-02-23 14:00:26 +0100

    avant slideshow

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
-  /*Start : Automatically start the slider */
+   
+//   $(function() {
+       
+          $('.timer').countTo({from: 0, to: 500});
+      
+//   });   
+   
+ smoothScroll.init();
+ 
+  new WOW().init();   
+  
+    
+//     $('#pricing').waypoint(function() {
+//     $('#tabe1').toggleClass('animated flipInY');
+//     $('#tabe2').toggleClass('animated flipInY');
+//     $('#tabe3').toggleClass('animated flipInY');
+//     $('#tabe4').toggleClass('animated flipInY');
+//   });
+ 
+ 
+ /*Start : Automatically start the slider */
 	$('.carousel').carousel({
       interval: 3000
    });
-	/*End: Automatically start the slider */
+ /*End: Automatically start the slider */
         
-        smoothScroll.init();
+	
