4a25ea8786ac1f8246c897e5b9578751ff8ef05a
kane <kanedev@live.fr>
2015-03-09 06:00:24 +0100

    Terminé

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -2,6 +2,11 @@
 // check if fields passed are empty
 require_once 'lib/swift_required.php';
 
+
+
+
+
+
 if(empty($_POST['name']) ||
 empty($_POST['email']) ||
 empty($_POST['message'])	||
@@ -16,21 +21,25 @@ return false;
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message_body = $_POST['message'];
-
+// Récup
+//$name = "lol";
+//$email_address = "mazarine333@yahoo.fr";
+//$message_body = "mon message";
+//
 
 
  
- $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
-  ->setUsername('YOUR ADRESSE GMAIL') //Change here 
-  ->setPassword('YOUR PASSWORD GMAIL') // change here
+ $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com',465, 'ssl')
+  ->setUsername('kzadev@gmail.com')  
+  ->setPassword('Casablanca1985') 
   ;
  
 // Create the message
 $message = Swift_Message::newInstance();
 $message->setTo(array(
-  "YOUR ADRESSE MAIL" => " SUBJECT" //Change here 
+  "kanedev@live.fr" => "SUBJECT" 
 ));
-$message->setSubject("Contact form submitted by: $name");
+$message->setSubject("Contact form submitted by : $name");
 $message->setBody("You have received a new message. \n\n".
 " Here are the details:\n \n Name: $name \n ".
 "Email: $email_address\n Message \n $message_body");
